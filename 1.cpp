e992b1cb8fd6dc37f1c31f8979ef681c472d5ce7
mirik-cyber <55018801+mirik-cyber@users.noreply.github.com>
2025-04-28 02:01:54 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -10,34 +10,34 @@ int lab2_init() {
     sem_init(&sem_n, 0, 0);
 
     // 1. a и c параллельно
-    pthread_create(&tid[0], NULL, thread_a, NULL);
-    pthread_create(&tid[2], NULL, thread_c, NULL);
+    pthread_create(&tid[0], NULL, thread_a, NULL); // a
+    pthread_create(&tid[2], NULL, thread_c, NULL); // c
 
-    // НЕ ЖДЁМ А! Только параллельный запуск.
+    pthread_join(tid[0], NULL); // Ждём ТОЛЬКО a!
 
-    // 2. b, e, g (после начала a, c)
-    pthread_create(&tid[1], NULL, thread_b, NULL);
-    pthread_create(&tid[4], NULL, thread_e, NULL);
-    pthread_create(&tid[6], NULL, thread_g, NULL);
+    // 2. b, e, g (после a, ПАРАЛЛЕЛЬНО с c)
+    pthread_create(&tid[1], NULL, thread_b, NULL); // b
+    pthread_create(&tid[4], NULL, thread_e, NULL); // e
+    pthread_create(&tid[6], NULL, thread_g, NULL); // g
 
-    // 3. d, f параллельно
-    pthread_create(&tid[3], NULL, thread_d, NULL);
-    pthread_create(&tid[5], NULL, thread_f, NULL);
+    // Теперь ждём ВСЮ группу: c, b, e, g
+    pthread_join(tid[2], NULL); // c
+    pthread_join(tid[1], NULL); // b
+    pthread_join(tid[4], NULL); // e
+    pthread_join(tid[6], NULL); // g
 
-    // Ждём ВСЕ: c, b, e, g, d, f
-    pthread_join(tid[2], NULL);
-    pthread_join(tid[1], NULL);
-    pthread_join(tid[4], NULL);
-    pthread_join(tid[6], NULL);
-    pthread_join(tid[3], NULL);
-    pthread_join(tid[5], NULL);
-    pthread_join(tid[0], NULL); // Теперь можно догнать "a"!
+    // 3. d, f параллельно (после завершения группы c,b,e,g)
+    pthread_create(&tid[3], NULL, thread_d, NULL); // d
+    pthread_create(&tid[5], NULL, thread_f, NULL); // f
 
-    // 4. h
+    pthread_join(tid[3], NULL); // d
+    pthread_join(tid[5], NULL); // f
+
+    // 4. h (после g)
     pthread_create(&tid[7], NULL, thread_h, NULL);
     pthread_join(tid[7], NULL);
 
-    // 5. i, k, m
+    // 5. i, k, m (после h)
     pthread_create(&tid[8], NULL, thread_i, NULL);
     pthread_create(&tid[9], NULL, thread_k, NULL);
     pthread_create(&tid[10], NULL, thread_m, NULL);
@@ -45,7 +45,7 @@ int lab2_init() {
     pthread_join(tid[9], NULL);
     pthread_join(tid[10], NULL);
 
-    // 6. n
+    // 6. n (после m)
     pthread_create(&tid[11], NULL, thread_n, NULL);
     pthread_join(tid[11], NULL);
 
@@ -61,3 +61,4 @@ int lab2_init() {
 
     return 0;
 }
+
