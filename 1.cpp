00ddf72c2555de4aac2110b0065f98ccf6ae2eda
mirik-cyber <55018801+mirik-cyber@users.noreply.github.com>
2025-04-30 01:18:27 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,55 +1,64 @@
-int lab2_init() {
-    pthread_mutex_init(&lock, NULL);
-    sem_init(&sem_i, 0, 1);
-    sem_init(&sem_k, 0, 0);
-    sem_init(&sem_m, 0, 0);
+// Copyright 2025 Miroslav
 
-    // 1. Старт a и c параллельно
-    pthread_create(&tid[0], NULL, thread_a, NULL); // a
-    pthread_create(&tid[2], NULL, thread_c, NULL); // c
+#include <cstdio>
+#include <cstring>
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 
-    pthread_join(tid[0], NULL); // Ждём только a, чтобы разрешить запуск b, e
+#include <iostream>
+#include <string>
 
-    // 2. Запуск b, e, g
-    pthread_create(&tid[1], NULL, thread_b, NULL); // b
-    pthread_create(&tid[4], NULL, thread_e, NULL); // e
-    pthread_create(&tid[6], NULL, thread_g, NULL); // g
+#define BUFFER_SIZE 512
 
-    pthread_join(tid[1], NULL); // b
-    pthread_join(tid[4], NULL); // e
+int main(int argc, char* argv[]) {
+  if (argc != 3) {
+    std::cerr << "Usage: ./client <server_ip> <port>\n";
+    return 1;
+  }
 
-    // 3. Запуск d, f (после b и e)
-    pthread_create(&tid[3], NULL, thread_d, NULL); // d
-    pthread_create(&tid[5], NULL, thread_f, NULL); // f
+  const char* server_ip = argv[1];
+  int port = std::stoi(argv[2]);
 
-    pthread_join(tid[3], NULL); // d
-    pthread_join(tid[5], NULL); // f
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock < 0) {
+    perror("socket");
+    return 1;
+  }
 
-    // Теперь ждём окончания c и g
-    pthread_join(tid[2], NULL); // c
-    pthread_join(tid[6], NULL); // g
+  sockaddr_in server_addr {};
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(port);
+  if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) <= 0) {
+    std::cerr << "Invalid address/ Address not supported\n";
+    return 1;
+  }
 
-    // 4. Запуск h
-    pthread_create(&tid[7], NULL, thread_h, NULL);
-    pthread_join(tid[7], NULL);
+  if (connect(sock, reinterpret_cast<sockaddr*>(&server_addr),
+              sizeof(server_addr)) < 0) {
+    perror("connect");
+    return 1;
+  }
 
-    // 5. Запуск i, k, m
-    pthread_create(&tid[8], NULL, thread_i, NULL);
-    pthread_create(&tid[9], NULL, thread_k, NULL);
-    pthread_create(&tid[10], NULL, thread_m, NULL);
+  std::string login, password;
+  std::cout << "Login: ";
+  std::getline(std::cin, login);
+  std::cout << "Password: ";
+  std::getline(std::cin, password);
 
-    pthread_join(tid[8], NULL);
-    pthread_join(tid[9], NULL);
-    pthread_join(tid[10], NULL);
+  std::string message = login + " " + password;
 
-    // 6. Запуск n
-    pthread_create(&tid[11], NULL, thread_n, NULL);
-    pthread_join(tid[11], NULL);
+  send(sock, message.c_str(), message.length(), 0);
 
-    pthread_mutex_destroy(&lock);
-    sem_destroy(&sem_i);
-    sem_destroy(&sem_k);
-    sem_destroy(&sem_m);
+  char buffer[BUFFER_SIZE] = {0};
+  int valread = recv(sock, buffer, BUFFER_SIZE - 1, 0);
+  if (valread > 0) {
+    buffer[valread] = '\0';
+    std::cout << "Server response: " << buffer;
+  } else {
+    std::cout << "No response from server.\n";
+  }
 
-    return 0;
+  close(sock);
+  return 0;
 }
