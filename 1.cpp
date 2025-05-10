1d6454c511b2d5431f28b8e34684dcf837104342
mirik-cyber <55018801+mirik-cyber@users.noreply.github.com>
2025-05-10 05:15:38 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,23 +1,13 @@
-#include <pthread.h>
-#include <semaphore.h>
-#include <iostream>
-#include <map>
-#include <string>
-
-pthread_mutex_t flag_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t flag_cond = PTHREAD_COND_INITIALIZER;
-std::map<std::string, bool> flags;
-
-void wait_flag(const std::string& name) {
-    pthread_mutex_lock(&flag_mutex);
-    while (!flags[name])
-        pthread_cond_wait(&flag_cond, &flag_mutex);
-    pthread_mutex_unlock(&flag_mutex);
-}
-
-void set_flag(const std::string& name) {
-    pthread_mutex_lock(&flag_mutex);
-    flags[name] = true;
-    pthread_cond_broadcast(&flag_cond);
-    pthread_mutex_unlock(&flag_mutex);
-}
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    enp0s3:  # ← имя интерфейса, замени на своё
+      dhcp4: no
+      addresses:
+        - 192.168.1.100/24
+      gateway4: 192.168.1.1
+      nameservers:
+        addresses:
+          - 8.8.8.8
+          - 8.8.4.4
